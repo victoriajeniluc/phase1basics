28bb9f3a912946e588b32d07b7fcf4e9d19ba0e5
Victoria Luc <victoriajeniluc@gmail.com>
2017-10-25 09:53:14 -0500

    create a value function for stampcollection

--- part-1/stamp_collection.js
+++ part-1/stamp_collection.js
@@ -2,3 +2,26 @@
 /*   It is required to use Array.prototype methods                */
 /*   when iterating over a stamp collections's array of stamps.   */
 /******************************************************************/
+
+var StampCollection = function(stamps) {
+  this.stamps = stamps;
+}
+
+StampCollection.prototype.stampNamed = function(nameWanted) {
+  return foundStamp = this.stamps.find(function(stamp){
+    return stamp.name === nameWanted;
+  });
+}
+
+StampCollection.prototype.stampsIssued = function(yearWanted) {
+  return foundStamps = this.stamps.filter(function(stamp){
+    return stamp.issueDate === yearWanted;
+  });
+}
+
+StampCollection.prototype.value = function() {
+  return totalValueOfStamps = this.stamps.reduce(function(sum, value){
+    return sum + value.maximumAppraisal();
+  },0);
+}
+
